f4c0a318c3ab36db64c0868093243919af048335
Ryan Burnham <burnharw@clarkson.edu>
2015-09-29 23:08:16 -0400

    a

--- Main.cpp
+++ Main.cpp
@@ -3,3 +3,13 @@
 #include <iostream>
 
 
+
+int main()
+{
+    std::string test_file_name = "testFile.txt";
+    std::string test_delimiter = "a";
+
+    int occurances = count_lines(test_file_name, test_delimiter);
+
+    std::cout << occurances << endl;
+}
