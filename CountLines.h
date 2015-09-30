4fbf119105a790221f9bbcd5d9272dbca2511e64
Ryan Burnham <burnharw@clarkson.edu>
2015-09-29 22:28:59 -0400

    Added the ability for the count_lines to simply count the number of lines in the file.

--- CountLines.h
+++ CountLines.h
@@ -6,7 +6,16 @@
 
 int count_lines(string file_name, string s)
 {
+    std::ifstream file(file_name);
+    int occurences;
 
+    while(file)
+    {
+         occurences++;
+         string current = std::getline(file);
+    }
+
+    return occurences;
 }
 
 #endif // COUNT_LINES
